33f3413718930be07461c6083d00d775b1fee2fe
Jack Liang <86848773+jack-200@users.noreply.github.com>
2025-05-31 22:02:17 -0700

    initial header box

--- digitalnest-product-catalog/src/App.tsx
+++ digitalnest-product-catalog/src/App.tsx
@@ -1,16 +1,16 @@
-import React, { useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   useEffect(() => {
-    document.title = 'DigitalNest Product Catalog';
+    document.title = "DigitalNest Shop";
   }, []);
 
   return (
     <>
-      <header>
-        <h1>DigitalNest Product Catalog</h1>
+      <header className="main-header-box">
+        <h1>DigitalNest Shop</h1>
       </header>
       <div>Homepage</div>
     </>
